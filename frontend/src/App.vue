dea76f2922d9cbac821d851c1e460700b2be0faf
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2025-08-11 16:56:32 +0300

    Ініціалізація Vue 3 + TypeScript проєкту, простий шаблон

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <h1>Привіт, Валитос! Це перший Vue 3 + TypeScript проект</h1>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>

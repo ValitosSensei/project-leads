08dfa22f601baf72685e8b79ec79918d493ac43a
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2025-08-14 14:30:31 +0300

    feat(lead): add validation form name+phone

--- frontend/src/components/ContactForm.vue
+++ frontend/src/components/ContactForm.vue
@@ -16,9 +16,17 @@ const lead = reactive<LeadDTO>({
   comment:'',
 })
 
+const errors = reactive<{name?: string; phone?:string}>({})
 const message  =ref('');
 
+const validate = ()  =>{
+  errors.name = lead.name.trim() ? '':'Ім’я обов’язкове';
+  errors.phone = /^\d{5,30}$/.test(lead.phone) ? '': 'Телефон повинен містити від 5 до 30 цифр';
+  return !errors.name && !errors.phone;
+}
+
 const submitLead = async () =>{
+  if(!validate()) return;
   try{
     const response = await axios.post('http://localhost:8080/api/leads', lead)
     message.value = 'Заявка успішно надіслана!'
@@ -36,10 +44,12 @@ const submitLead = async () =>{
     <div>
       <label for="name">Ім'я</label>
       <input v-model="lead.name" id="name" required></input>
+      <div v-if="errors.name" style="color:red">{{ errors.name }}</div>
     </div>
     <div>
       <label for="phone">Телефон</label>
       <input v-model="lead.phone" id="phone" required></input>
+      <div v-if="errors.phone" style="color:red">{{ errors.phone }}</div>
     </div>
     <div>
       <label>Тип роботи:</label>
